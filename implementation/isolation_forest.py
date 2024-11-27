ec4bf3732e67a4eb6ad25722769aa5c577185a02
sanjeevs <sanjeevs@msoe.edu>
2024-11-27 13:02:30 -0600

    isolation_tree and isolation_forest

---
+++ implementation/isolation_forest.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+import scipy as sc
+from isolation_tree import IsolationTree
+
+class IsolationForest:
+    # implements Algorithm 1: iForest from original Isolation Forest paper
+
+    def __init__(self, t, sub_sampling_size):
+        self.t = t # number of trees
+        self.sub_sampling_size = sub_sampling_size # sub sampling size ψ
+        self.forest = None
+
+    def fit(self, X:np.array):
+        # initialize forest - stores isolation trees
+        self.forest = []
+        # set height limit using paper's formula
+        self.height_limit = math.ceil(math.log2(self.sub_sampling_size))
+        # repeat t times:
+        for i in range(self.t):
+            # X' ← sample(X, ψ) [randomly select subset of ψ samples from dataset X (subset denoted as X')
+            if self.sub_sampling_size < X.shape[0]:
+                X_subsample = X[np.random.choice(X.shape[0], self.subsample_size, replace=False)]
+            else:
+                X_subsample = X  # if subsample size > data size, use all data
+            # Forest ← Forest ∪ iTree(X', 0, l)
+            # Build an IsolationTree with X' ss the inputdata, starting height = 0, and height limit = l
+            # then add the IsolationTree to the forest
+            tree = IsolationTree(e=0, limit=self.limit).fit(X_subsample)
+            self.forest.append(tree)
+        # output is forest (in self.forest list)    
+        return self
--- implementation/isolation_tree.py
+++ implementation/isolation_tree.py
@@ -2,11 +2,16 @@ import numpy as np
 import scipy as sc
 
 class IsolationTree:
-    # implements Algorithm 2: iTree from original paper
+    # implements Algorithm 2: iTree from original Isolation Forest paper
 
     def __init__(self, e, limit):
         self.e = e # current tree height
         self.limit = limit # height limit
+        self.right_subtree = None
+        self.left_subtree = None
+        self.split_point_p = None
+        self.attribute_q = None
+        self.size = None
 
     def fit(self, X:np.array):
 
@@ -18,14 +23,35 @@ class IsolationTree:
         else:
             # Q is list of attributes in X (data)
             # choose random attribute q i.e. feature from data
-            self.random_attribute = self.randomly_select_attribute(X)
-            # pick random split value p within range of value for attrivutes
-            pass
+            self.attribute_q = self.randomly_select_attribute(X)
+            # pick random split value p within range of value for attribute
+            attribute_q_data = X[:,self.attribute_q]
+            self.split_point_p = self.randomly_select_split_point(attribute_q_data)
+            # split data into left and right based on the split point
+            # X_l ← filter(X, q < p)
+            X_left = X[attribute_q_data < self.split_point_p]
+            # X_r ← filter(X, q ≥ p)
+            X_right = X[attribute_q_data >= self.split_point_p]
+            # recursive splitting
+            # build left subtree increasing current height to e + 1
+            self.left_subtree = IsolationTree(self.e + 1, self.limit).fit(X_left)
+            # also build right subtree
+            self.right_subtree = IsolationTree(self.e + 1, self.limit).fit(X_right)
+            # store random split attribute q and split value p in an internal Node
+            #TODO?
+            # output: an iTree
+            return self
 
     def randomly_select_attribute(self, X):
         # X is data
         # randomly select an attribute (column index) from data
         return np.random.randint(0, X.shape[1])
 
-    def randomly_select_split_point(self):
-        pass
+    def randomly_select_split_point(self, attribute_q_data):
+        # randomly select a split point p from max and min values of attribute q in X
+        # attribute_q_data -> 1D array of values for randomly selected attribute q
+        # retruns a randomly selected split point
+        min_value = attribute_q_data.min()
+        max_value = attribute_q_data.max()
+        split_point = np.random.uniform(min_value, max_value)
+        return split_point
